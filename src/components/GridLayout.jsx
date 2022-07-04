eec23d76d0c3d75f267bca76639524faf733cdd9
Fernando Castellanos <fernan2cas96@gmail.com>
2022-07-04 11:56:38 -0400

    Creado: soyn un boton

---
+++ src/components/GridLayout.jsx
@@ -0,0 +1,18 @@
+const GridLayout = () => { 
+
+    return (
+      <div className="grid-container">
+        <div className="item1">Soy un boton</div>
+        <div className="item2">2</div>
+        <div className="item3">3</div>
+        <div className="item4">4</div>
+        <div className="item5">5</div>
+        <div className="item6">6</div>
+        <div className="item7">7</div>
+        <div className="item8">8</div>
+        <div className="item9">9</div>
+      </div>
+    );
+}
+
+export default GridLayout
---
+++ src/components/Section1.jsx
@@ -0,0 +1,9 @@
+const Section1 = () => { 
+    return (
+        <section className="section1">
+            fsdfs
+        </section>
+    )
+}
+
+export default Section1
